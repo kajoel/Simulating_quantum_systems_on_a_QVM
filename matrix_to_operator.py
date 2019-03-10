025c4cb6da3b41b7642e1b19612d68ad07446b86
Joel Karlsson <kajoel@student.chalmers.se>
2019-03-10 11:24:40 +0100

    Doc fix

--- matrix_to_operator.py
+++ matrix_to_operator.py
@@ -7,6 +7,7 @@ Created on Tue Mar  5 08:14:54 2019
 """
 import scipy.sparse as sparse
 import numpy as np
+import pyquil.paulis
 from openfermion.ops import FermionOperator
 from openfermion.ops import QubitOperator
 from forestopenfermion import qubitop_to_pyquilpauli
@@ -17,21 +18,19 @@ from openfermion.transforms import jordan_wigner
 # VQE_RELATED FUNCTIONS
 ###############################################################################
 
-"""
-Creates a Hamiltonian operator from a (sparse) matrix. This function uses
-a one-particle formulation and, thus, requires N qubits for an N-dimensional
-Hilbert space.
 
-@author: axelnathanson
-"""
-def matrix_to_operator_1(H):
-    """[Generates a PauliSum(pyquil) given a Hamiltonian-matrix]
-    
-    Arguments:
-        H {[sparse.matrix]} -- [Hamiltonian matrix]
-    
-    Returns:
-        [PauliSum] -- [Matrix representation of pauli-operators in pyquil]
+def matrix_to_operator_1(H: sparse.coo_matrix) -> pyquil.paulis.PauliSum:
+    """
+    Generates a PauliSum(pyquil) given a Hamiltonian-matrix.
+
+    Creates a Hamiltonian operator from a (sparse) matrix. This function uses
+    a one-particle formulation and, thus, requires N qubits for an N-dimensional
+    Hilbert space.
+
+    @author: axelnathanson
+
+    :param H: An array (array_like) representing the hamiltonian.
+    :return: Hamiltonian as PyQuil PauliSum.
     """
     # Create the Hamiltonian with a and a^dagger
     Hamiltonian = FermionOperator()
@@ -40,25 +39,27 @@ def matrix_to_operator_1(H):
         for i, j, data in zip(H.row, H.col, H.data):
             Hamiltonian += data * FermionOperator(((int(i), 1), (int(j), 0)))
     else:
+        if not isinstance(H, np.ndarray):
+            H = np.asanyarray(H)
         for i in range(H.shape[0]):
             for j in range(H.shape[1]):
-                Hamiltonian += H[i,j]*FermionOperator(((i,1), (j,0)))
+                Hamiltonian += H[i, j] * FermionOperator(((i, 1), (j, 0)))
 
     Hamiltonian = jordan_wigner(Hamiltonian)
     Hamiltonian = qubitop_to_pyquilpauli(Hamiltonian)
     return Hamiltonian
 
 
-def matrix_to_operator_2(H):
+def matrix_to_operator_2(H: sparse.coo_matrix) -> pyquil.paulis.PauliSum:
     """
     Creates a Qubit-operator from a (sparse) matrix. This function uses
-    (almost) all states and, thus, requires (approximately) log(N) quibits
+    (almost) all states and, thus, requires (approximately) log(N) qubits
     for an N-dimensional Hilbert space.
 
     The idea for converting a matrix element to an operator is to raise/lower
     the qubits differing between two states to convert one basis-state to the
     other. The qubits that are not negated must be checked to have the correct
-    value (using an analogue to the counting operator) to not get extra terms
+    value (using an analogue of the counting operator) to not get extra terms
     (one could perhaps allow for extra terms and compensate for them later?).
 
     0 = up
@@ -70,6 +71,9 @@ def matrix_to_operator_2(H):
     (Xi-1j*Yi)/2 raises qubit from 0 (up) to 1 (down)
 
     @author: kajoel
+
+    :param H: An array (array-like) representing the hamiltonian.
+    :return: Hamiltonian as PyQuil PauliSum.
     """
     # Convert to sparse coo_matrix
     if not sparse.issparse(H):
@@ -85,13 +89,14 @@ def matrix_to_operator_2(H):
                 # lower/raise qubit
                 new_term *= QubitOperator((qubit, "X"), 1 / 2) + \
                             QubitOperator((qubit, "Y"),
-                                          1j * (int(j & (1 << qubit) != 0) - 1 / 2))
+                                          1j * (int(
+                                              j & (1 << qubit) != 0) - 1 / 2))
             else:
                 # check that qubit has correct value (same as i and j)
                 new_term *= QubitOperator((), 1 / 2) + \
                             QubitOperator((qubit, "Z"),
-                                          1/2-int(j & (1 << qubit) != 0))
-        H_op += data*new_term
+                                          1 / 2 - int(j & (1 << qubit) != 0))
+        H_op += data * new_term
     return qubitop_to_pyquilpauli(H_op)
 
 
@@ -138,8 +143,8 @@ def _test_mat_to_op(hamiltonian_operator, jmin=0.5, jmax=100, tol=1e-8):
 # MAIN
 ###############################################################################
 if __name__ == "__main__":
-    print(test)
-    #_ test_mat_to_op(matrix_to_operator_1, jmax=10)
+    pass
+    #  test_mat_to_op(matrix_to_operator_1, jmax=10)
     # The following tests has (successfully) been completed:
     # _test_mat_to_op(matrix_to_operator_1)
     # _test_mat_to_op(matrix_to_operator_1, jmax=11)
