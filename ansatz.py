6938e52425a37fce2656a22c839aa8c4d950ccbe
Joel Karlsson <kajoel@student.chalmers.se>
2019-03-11 20:03:04 +0100

    Multi-particle ansatz and comparing the ansätze
    # gates grows exponentially for one_particle_ansatz but linearly for multiple_particle_ansatz (gate depth! This is probably more of an issue than the growing terms in the Hamiltonian.)

--- ansatz.py
+++ ansatz.py
@@ -9,13 +9,64 @@ Created on Mon Mar  4 10:50:49 2019
 
 # Imports
 import numpy as np
+from pyquil.quil import Program
 from grove.alpha.arbitrary_state.arbitrary_state import create_arbitrary_state
 
 
-def one_particle_ansatz(theta):
+def one_particle_ansatz(theta: np.ndarray) -> Program:
     """
-    TODO: doc
+    Creates a program to set up an arbitrary one-particle-state.
+
+    :param theta: Vector representing the state.
+    :return: PyQuil program setting up the state.
     """
+    # TODO: doc behaves weird, theta: Union[ndarray, ndarray]
     vector = np.zeros(2 ** (theta.shape[0]))
     vector[[2 ** i for i in range(theta.shape[0])]] = theta
     return create_arbitrary_state(vector)
+
+
+def multi_particle_ansatz(theta: np.ndarray) -> Program:
+    """
+    Creates a program to set up an arbitrary state.
+
+
+    :param theta: Vector representing the state.
+    :return: PyQuil program setting up the state.
+    """
+    # TODO: doc behaves weird, theta: Union[ndarray, ndarray]
+    return create_arbitrary_state(theta)
+
+
+################################################################################
+# TESTS
+################################################################################
+def _test_depth(ansatz, n_min=1, n_max=12, m=5):
+    nn = n_max - n_min + 1
+    nbr_ops = np.zeros(nn)
+    for i in range(nn):
+        n = n_min + i
+        print(n)
+        temp = np.empty(m)
+        for j in range(m):
+            temp[j] = len(ansatz(np.random.randn(n)))
+        nbr_ops[i] = np.average(temp)
+    return nbr_ops
+
+
+################################################################################
+# MAIN
+################################################################################
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    nbr_ops_s = _test_depth(one_particle_ansatz)
+    nbr_ops_m = _test_depth(multi_particle_ansatz, n_max=2 ** 5)
+
+    plt.figure(0)
+    plt.plot(nbr_ops_s)
+    plt.title("Number of gates in one-particle-ansatz")
+
+    plt.figure(1)
+    plt.plot(nbr_ops_m)
+    plt.title("Number of gates in multi-particle-ansatz")
