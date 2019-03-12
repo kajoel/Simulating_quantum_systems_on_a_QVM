39f3d6393d52b3c2e83a7e5c17a7d21de1a1700e
Eric Nilsson <nieric@student.chalmers.se>
2019-03-12 09:12:34 +0100

    Changed deafualt ansatz params

--- main.py
+++ main.py
@@ -9,32 +9,39 @@ from vqe_eig import calculate_eigenvalues_vqe, smallest_eig_vqe
 from lipkin_quasi_spin import hamiltonian, eigenvalues
 from ansatz import one_particle_ansatz
 import pprint
+import time
 import numpy as np
 import pyquil.api as api
 import time
 
 qvm = api.QVMConnection()
 
-
+j = 4
 V = 1
-
-
-'''
-energies = calculate_eigenvalues_vqe(TestHamiltonian, one_particle_ansatz,qvm)
+H = hamiltonian(j, V)
+Realenergies = eigenvalues(j, V)
+TestHamiltonian = H[1].toarray()
+# energies = calculate_eigenvalues_vqe(TestHamiltonian, one_particle_ansatz)
+start = time.time()
+energies = smallest_eig_vqe(TestHamiltonian, one_particle_ansatz, qvm, num_samples=10)[0]
+end = time.time()
 pprint.pprint([round(x, 3) for x in Realenergies[1].tolist()])
-pprint.pprint([round(x, 3) for x in sorted(energies)])
-'''
+# pprint.pprint([round(x, 3) for x in sorted(energies)])
+pprint.pprint(energies)
+print('time: \n:', end - start)
 
+'''
 timer = []
-for j in range(1,10):
+for j in range(1, 3):
     t1 = time.time()
     H = hamiltonian(j, V)
     TestHamiltonian = H[1]
-    smallest_eig_vqe(TestHamiltonian, one_particle_ansatz,qvm)[0]
+    val = smallest_eig_vqe(TestHamiltonian, one_particle_ansatz, qvm, num_samples=10)[0]
+    print(val)
     t2 = time.time()
     timer.append(t2-t1)
 
 print('j \t Time elapsed [s]\n')
-for index,t in enumerate(timer):
-    print('{} \t {}\n'.format(index+1,t))
-
+for index, t in enumerate(timer):
+    print('{} \t {}\n'.format(index+1, t))
+'''
