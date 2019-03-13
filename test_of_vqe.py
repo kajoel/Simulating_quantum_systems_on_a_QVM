3090b76640b7f25d3d27a5c2bd2ade776a73a98e
Axel <axel.nathanson@gmail.com>
2019-03-13 18:07:47 +0100

    Fixed indentation and comments in test_of_vqe
    Also erased som old files, not relevant any more since i benchmarked som runs with our old ansatz and no samples.

--- test_of_vqe.py
+++ test_of_vqe.py
@@ -31,37 +31,46 @@ from ansatz import one_particle_inital as initial
 
 
 
-def count_opt_iterations(H, qvm, old_version = False,  samples=None, disp_run_info=False, 
-                         return_dict = False, xatol=1e-2,fatol=1e-3,maxiter=10000):
+def count_opt_iterations(H, qvm, old_version = False,  samples=None, 
+                         disp_run_info=False, return_dict=False, xatol=1e-2, 
+                         fatol=1e-3,maxiter=10000):
     """Count the number of iterations the Nelder-Mead takes to converge
     Arguments:
-        H {Matrix} -- Hamiltonian matrix
-        qvm {quantum computer} -- QMVConnection as of right now
+        :param H: Hamiltonian matrix
+        :param qvm: Quantum computer
     
     Keyword Arguments:
-        old_version {bool} -- True if you are using qvm, Fase if qc
-        samples {int} -- Number of samples (default: {None})
-        disp_run_info {bool} -- Display output in each step of optimization (default: {False})
-        xatol {float} -- error in x_opt between iterations for convergence (default: {1e-2})
-        fatol {float} -- error in f(x_opt) between iterations for convergence (default: {1e-3})
-        maxiter {int} -- Maximum number of iterations before termination (default: {10000})
-        return_dict {bool} -- Returns the entire dict with all data from every iteration
+        :param old_version: bool if you are using a old version. default:False
+        :param samples: int representing number of samples. default:None
+        :param disp_run_info: bool, if true prints all intermidiate 
+                              calculated values. default: False
+        :param xatol: float, error in x_opt between Nelder-M iterations
+        :param xatol: float, error in f(x_opt) between Nelder-M iterations
+        :param maxiter: int, iterations before termination. default:10000
+        :param return_dict: bool, if True return data of all iterations
     Returns:
-        iterations {int} -- Number of iterations before convergence
-        if return_dict is True: Returns dict with all run-data
+        Numer of iterations, or the dict with data of all iterations.
+        
     """                     
 
     Option = {'xatol': xatol, 'fatol': fatol, 'maxiter': maxiter}
-    vqe = VQE(minimizer=minimize, minimizer_kwargs={'method': 'Nelder-Mead','options': Option})
+    vqe = VQE(minimizer=minimize, minimizer_kwargs={'method': 'Nelder-Mead',
+              'options': Option})
+
     H_program = matrix_to_operator_1(H)
     
     if disp_run_info: display_option = print
     else: display_option = lambda x:None
     
-    if old_version: result = vqe.vqe_run(ansatz, H_program, initial(H.shape[0]), samples=samples, 
-                             qvm=qvm, disp=display_option, return_all=True)
-    else: result = vqe.vqe_run(ansatz, H_program, initial(H.shape[0]), samples=samples, 
-                             qc=qvm, disp=display_option, return_all=True)
+    if old_version:
+        result = vqe.vqe_run(ansatz, H_program, initial(H.shape[0]), 
+                             samples=samples, qvm=qvm, disp=display_option, 
+                             return_all=True)
+
+    else: 
+        result = vqe.vqe_run(ansatz, H_program, initial(H.shape[0]), 
+                               samples=samples, qc=qvm, disp=display_option, 
+                               return_all=True)
 
     
     print('Real eigenvalues:')
@@ -73,12 +82,11 @@ def count_opt_iterations(H, qvm, old_version = False,  samples=None, disp_run_in
     else: return len(result['iteration_params'])
 
 
-def sweep_parameters(H, qvm, old_version=False, num_para=20, start = -10, stop = 10,samples = None, 
-                    fig_nr = 0, save = False):
-    
+def sweep_parameters(H, qvm, old_version=False, num_para=20, start = -10, 
+                     stop = 10,samples = None, fig_nr = 0, save = False):
     
     '''
-    TODO: Add a statement that saves the data from the run, when the para save=True
+    TODO: Add a statement that saves the data from the run.
     '''
     vqe = VQE(minimizer=minimize, minimizer_kwargs={'method': 'Nelder-Mead'})
     if H.shape[0] > 3:
@@ -92,9 +100,13 @@ def sweep_parameters(H, qvm, old_version=False, num_para=20, start = -10, stop =
         parameters = np.linspace(start,stop,num_para)
 
         if old_version:
-            exp_val = [vqe.expectation(ansatz(np.array([para])), H, samples = samples, qvm=qvm) for para in parameters]
+            exp_val = [vqe.expectation(ansatz(np.array([para])), H, 
+                                       samples=samples, qvm=qvm) 
+                                       for para in parameters]
         else:
-            exp_val = [vqe.expectation(ansatz(np.array([para])), H, samples = samples, qc=qvm) for para in parameters]
+            exp_val = [vqe.expectation(ansatz(np.array([para])), H, 
+                                       samples=samples, qc=qvm) 
+                                       for para in parameters]
         
 
         plt.figure(fig_nr)
@@ -113,11 +125,13 @@ def sweep_parameters(H, qvm, old_version=False, num_para=20, start = -10, stop =
             mesh_1[i]+=p_1
             mesh_2[i] = parameters
             if old_version:
-                exp_val[i] = [vqe.expectation(ansatz( np.array([p_1,p_2]) ), H, samples = samples, qvm=qvm)
-                              for p_2 in parameters]
+                exp_val[i] = [vqe.expectation(ansatz( np.array([p_1,p_2]) ), H, 
+                                              samples = samples, qvm=qvm)
+                                              for p_2 in parameters]
             else:
-                exp_val[i] = [vqe.expectation(ansatz( np.array([p_1,p_2]) ), H, samples = samples, qc=qvm)
-                              for p_2 in parameters]  
+                exp_val[i] = [vqe.expectation(ansatz( np.array([p_1,p_2]) ), H, 
+                                              samples = samples, qc=qvm)
+                                              for p_2 in parameters]  
                                           
         fig = plt.figure(fig_nr)
         ax = fig.add_subplot(111, projection='3d')
@@ -128,11 +142,10 @@ def sweep_parameters(H, qvm, old_version=False, num_para=20, start = -10, stop =
 
 
 def save_run_to_csv(Variable):
-    """ Save given variable to CSV-file with name of exakt time down to mili-sec
+    """ Save given variable to CSV-file with name of exakt time
     Arguments:
-        Variable {np.array} -- Variable you want to save to .txt file in CSV-format
+        Variable{np.array}--Variable to save to .txt file in CSV-format
     """
-    
     np.savetxt('{}'.format(datetime.now()),Variable)
 
 
@@ -157,11 +170,15 @@ def main2(samples=1000, sweep_params=100):
     qvm = api.QVMConnection()
     j,V = 1,1
     H,_ = hamiltonian(j,V)
-    sweep_parameters(H,qvm,old_version = True, samples=samples, num_para=sweep_params, start=-2,stop=2)
+    sweep_parameters(H,qvm,old_version = True, samples=samples, 
+                     num_para=sweep_params, start=-2,stop=2)
     
-    result = count_opt_iterations(H,qvm, old_verion=True, samples=samples,fatol=1e-2,xatol=1e-3, return_dict=True)
+    result = count_opt_iterations(H,qvm, old_verion=True, samples=samples,
+                                  fatol=1e-2,xatol=1e-3, return_dict=True)
     
-    plt.scatter(result['iteration_params'],result['expectation_vals'], label='Steps in nelder mead')
+    plt.scatter(result['iteration_params'],result['expectation_vals'], 
+                label='Steps in nelder mead')
+
     plt.legend()
 
     plt.show()
