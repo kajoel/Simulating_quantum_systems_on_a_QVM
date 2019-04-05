9d3dfed28440d7b7ad5325996d836eb36c4bfb26
Joel Karlsson <kajoel@student.chalmers.se>
2019-04-05 20:34:44 +0200

    Failsafe save

--- core/data.py
+++ core/data.py
@@ -10,16 +10,18 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilename, askopenfilenames, \
     asksaveasfilename
 from datetime import datetime
-from os.path import basename, join
+from os.path import basename, join, dirname, isdir
 from inspect import stack, getmodule
-from os import getuid
+from os import getuid, mkdir
 from pwd import getpwuid
 from constants import ROOT_DIR
+from traceback import format_exc
 
 USER_PATH = join(ROOT_DIR, 'data', 'users.pkl')
 
 
-def save(file=None, data=None, metadata=None):
+def save(file=None, data=None, metadata=None,
+         base_dir=join(ROOT_DIR, 'data')):
     """
     Save data and metadata to a file. Always set the first field of metadata
     as metadata={'description': info} (followed by other fields). Datetime of
@@ -34,19 +36,23 @@ def save(file=None, data=None, metadata=None):
     @author = Joel
 
     :param string file: file to save to
+    :param string base_dir: prepended to file
     :param data: data to save
     :param dictionary  metadata: metadata describing the data
     :return: None
     """
-    # TODO: Failsafe save ASAP (handle errors)
-    # UI get file if is None.
+    # UI get file if is None and create directory if not existing.
     if file is None:
         tk = Tk()
         tk.withdraw()
-        file = asksaveasfilename(parent=tk, filetypes=[('Pickled', '.pkl')],
-                                 initialdir=join(ROOT_DIR, 'data'))
-    if file is None:
-        raise FileNotFoundError("Can't save without a file.")
+        file = asksaveasfilename(parent=tk, filetypes=[('Pickled', '.pkl'),
+                                                       ('All files', '.*')],
+                                 initialdir=base_dir)
+    else:
+        file = join(base_dir, file)
+        dir_ = dirname(file)
+        if not isdir(dir_):
+            mkdir(dir_)
 
     # Add some fields automatically to metadata
     if metadata is None:
@@ -55,18 +61,23 @@ def save(file=None, data=None, metadata=None):
                      _metadata_defaults().items() if key not in
                      metadata})
 
-    # Make sure that data is dictionary
+    # Make sure that data is a dictionary
     if data is None:
         data = {}
     if not isinstance(data, dict):
         data = {'data': data}
 
-    # Display
-    _display_internal(file, data, metadata)
-
     # Save
-    with open(file, 'wb') as file_:
-        pickle.dump({'data': data, 'metadata': metadata}, file_)
+    _failsafe_save(file, data, metadata)
+
+    # Display
+    try:
+        _display_internal(file, data, metadata)
+    except:
+        print('\033[93m' + 'The following error occurred while trying to '
+                           'display metadata from the saved file:'
+              + '\033[0m\n')
+        print('\033[91m' + format_exc() + '\033[0m')
 
 
 def load(file=None):
@@ -83,7 +94,8 @@ def load(file=None):
     if file is None:
         tk = Tk()
         tk.withdraw()
-        file = askopenfilename(parent=tk, filetypes=[('Pickled', '.pkl')],
+        file = askopenfilename(parent=tk, filetypes=[('Pickled', '.pkl'),
+                                                     ('All files', '.*')],
                                initialdir=join(ROOT_DIR, 'data'))
     if file is None:
         raise FileNotFoundError("Can't load without a file.")
@@ -106,7 +118,8 @@ def display(files=None):
     if files is None:
         tk = Tk()
         tk.withdraw()
-        files = askopenfilenames(parent=tk, filetypes=[('Pickled', '.pkl')],
+        files = askopenfilenames(parent=tk, filetypes=[('Pickled', '.pkl'),
+                                                       ('All files', '.*')],
                                  initialdir=join(ROOT_DIR, 'data'))
     elif isinstance(files, str):
         files = [files]
@@ -216,17 +229,108 @@ def _metadata_defaults():
     :return: metadata
     :rtype: dictionary
     """
+
     def get_caller():
         try:
             caller = basename(getmodule(stack()[3][0]).__file__)
         except:
             caller = 'unknown'
         return caller
+
     return {'created_by': _get_name,
             'created_from': get_caller,
             'created_datetime': lambda: datetime.now().strftime("%Y-%m-%d, "
                                                                 "%H:%M:%S"),
             }
+
+
+def _failsafe_save(file, data, metadata):
+    """
+    Method for (hopefully) failsafe saving.
+
+    @author = Joel
+
+    :param str file: file to save to
+    :param data: data to save
+    :param metadata: metadata describing the data
+    :return:
+    """
+    modifiable = ['file', 'data', 'metadata']
+    locals_ = {'file': file, 'data': data, 'metadata': metadata}
+    exit_ = False
+    while not exit_:
+        try:
+            with open(file, 'wb') as file_:
+                pickle.dump({'data': data, 'metadata': metadata}, file_)
+        except Exception as e:
+            locals_['e'] = e
+            print('\n\033[1m\033[93m' + 'Failed while saving. Handing the '
+                                        'control to you.' + '\033[0m')
+            print('\033[93m' + 'The following error occurred and can be '
+                               'accessed as e:' + '\033[0m\n')
+            print('\033[91m' + format_exc() + '\033[0m')
+            print('\033[94m\033[1m\033[4m' + 'Instructions:' + '\033[0m')
+            print('\033[94m\033[1m' + '%retry' + '\033[0m\033[94m' +
+                  ' will cause the program to try to save again' + '\033[0m')
+            print('\033[94m\033[1m' + '%exit' + '\033[0m\033[94m' +
+                  ' will cause the program to quit without saving' + '\033[0m')
+            print('\033[94m\033[1m' + '%skip' + '\033[0m\033[94m' +
+                  ' (as the only thing on any line) will cause the program to '
+                  'not \n\texecute anything of what you have entered' +
+                  '\033[0m')
+            print('\033[94m\033[1m' + '[blank line]' + '\033[0m\033[94m' +
+                  ' will cause the program to execute the above lines\n' +
+                  '\t(if not containing %skip)' + '\033[0m')
+            print('\n\033[92m' + "You have access to the following "
+                                 "variables \n(the ones in bold will be "
+                                 "modified in data.save's scope):\n"
+                  + ''.join('\t\033[92m' + '\033[1m' * (key in modifiable) + key
+                            + '\033[0m' for key in locals_)
+                  + '\033[0m')
+            retry = False
+            while not retry and not exit_:
+                lines = _read_input()
+                skip_str = '\n%skip\n'
+                retry_str = '\n%retry\n'
+                exit_str = '\n%exit\n'
+                skip = skip_str in lines
+                retry = (retry_str in lines) and not skip
+                exit_ = (exit_str in lines) and not retry and not skip
+                lines = lines.replace(skip_str, '\n'). \
+                    replace(retry_str, '\n').replace(exit_str, '\n')
+                if not skip:
+                    try:
+                        exec(lines, {}, locals_)
+                    except:
+                        print('\n\033[1m\033[93m' + 'Failed while executing:' +
+                              '\033[0m')
+                        print('\033[91m' + format_exc() + '\033[0m')
+                        print('\033[93m' + 'You can try again.' + '\033[0m')
+                        retry = False
+                        exit_ = False
+            file = locals_['file']
+            data = locals_['data']
+            metadata = locals_['metadata']
+        else:
+            exit_ = True
+
+
+def _read_input():
+    """
+    Reads multiple lines from input (until blank line).
+
+    @author = Joel
+
+    :return: inputted lines
+    """
+    lines = ''
+    prompt = '>>> '
+    line = input(prompt)
+    prompt = '... '
+    while line:
+        lines += '\n' + line
+        line = input(prompt)
+    return lines + '\n'
 
 
 '''
