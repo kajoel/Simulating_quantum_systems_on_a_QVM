d8e9796810137d8716d7205b5f2995cd5a4d3517
Axel <axel.nathanson@gmail.com>
2019-04-05 11:44:57 +0200

    Fixed name and date

--- meas/run_bayes_smallest.py
+++ meas/run_bayes_smallest.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tus Mar 26 10:06 2019
+Created on Fri April 5 11:06 2019
 
 @author: axel
 """
@@ -11,7 +11,7 @@ from pyquil import get_qc
 import matplotlib.pyplot as plt
 
 def run_bayes_opt():
-    j,V = 3,1
+    j,V = 2,1
 
     h = lipkin_quasi_spin.hamiltonian(j,V)[0]
     print(h)
