2bf1c014654b1f29cdd75b025611a1e9afbcaf2c
Joel Karlsson <kajoel@student.chalmers.se>
2019-04-22 21:43:46 +0200

    New recipe almost done

--- meas/parallel_recipe.py
+++ meas/parallel_recipe.py
@@ -4,7 +4,7 @@ single file.
 
 @author = Joel
 """
-from core.data import save, load
+from core import data
 from os.path import join, basename
 import os
 from multiprocessing import Pool
@@ -14,14 +14,17 @@ from core import lipkin_quasi_spin
 # TODO: (for Joel) check if pauli-objects are thread-safe (they probably are)
 #  write new save style and append (with open(file, 'ab')) in data (using
 #       multiple objects in pickle)
-#  write example input_iterate() and simulate()
 #  write class that takes iterator and set and wraps it to iterate through
 #       the iterator but only yield the second output if the first is not in
 #       the set (in that case, add the first to the set and save to metafile)
+#  change metadata['ids'] to a set with identifiers corresponding to
+#  previously completed tasks (use protocol=2 in data.save and load).
 #  cache output_func
 
 
 # TODO: When writing a meas script, change (only) the parts marked by TODOs.
+#  MAKE SURE TO SAFE ENOUGH INFORMATION!
+#  BETTER TO SAVE TOO MUCH THAN TOO LITTLE!
 
 # TODO: give a version-number of the script (this should be changed iff the
 #  meaning of the elements in the tuple yielded by the generator (
@@ -31,9 +34,9 @@ from core import lipkin_quasi_spin
 #  file name (e.g test_v1_...)
 version = 1
 
-# TODO: select directory and file to save to (the case is appended to the file).
+# TODO: select directory and basename of file to save to.
 directory = 'test_recipe'  # directory to save to
-file = 'test'  # file to save to
+file = 'test'  # file to save to (basename)
 
 # Append version number to file
 file += f'_v{version}'
@@ -45,82 +48,105 @@ path_metadata = join(directory, file + '_metadata')
 # Load/initialize metadata
 try:
     # Try to load the file (will raise FileNotFoundError if not existing)
-    metadata = load(path_metadata)[0]
+    metadata = data.load(path_metadata)[0]
 except FileNotFoundError:
-    metadata = {'identifiers': {}}
+    metadata = {'ids': {}}
     metametadata = {'description': "File that keeps track of what's been done "
                                    "previously in this script "
                                    f"({basename(__file__)})."}
-    save(file=path_metadata, data=metadata, metadata=metametadata)
+    data.save(file=path_metadata, data=metadata, metadata=metametadata)
 
 
 # TODO: identifiers to iterate over, should yield a tuple that uniquely
-#  identifies the simulation (using floats/ints as elements is a good idea),
-#  e.g (1, 2, 3, 4)
+#  identifies the simulation (using floats/ints as elements is a good idea,
+#  e.g (1, 2, 3, 4)), i.e. every call to smallest, vqe.run or similar should
+#  have a unique identifier.
 def identifier_generator():
+    # Example:
+    # input_0 is effectively called here without arguments
     V = 1.
+    # input_1 is effectively called here with one argument
     for j in range(6):
+        # input_2 is effectively called here with two arguments
         for i in range(2):
+            # input_3 is effectively called here with three arguments
             for samples in range(100, 1000, 100):
+                # input_4 is effectively called here with four arguments
                 for num_sim in range(5):
+                    # input_5 is effectively called here with five arguments
                     yield (V, j, i, samples, num_sim)
 
 
-# TODO
+# TODO: Functions for creating objects (things larger than ints/floats) that
+#  simulate (below) can use. This is a good place to create e.g. the
+#  hamiltonian. input_n will get the first n items in the identifiers. Try to
+#  setup things as early as possible (better to do it in input_0 than input_1
+#  and so on) to avoid creating identical objects (and save some time). These
+#  have to return an object that can be iterated over (like a tuple or list).
+#  You do not have to define all (or any) of input_0, input_1, ...
 def input_0():
-    # could return e.g ansatz
-    return [None]
+    # (Poor) example (you probably don't want to return None)
+    return None,  # Note the ',' (comma sign) which makes the output a tuple
 
 
-# TODO
 def input_3(V, j, i):
+    # Example of setting up the hamiltonian.
     return [lipkin_quasi_spin.hamiltonian(V, j)[i]]
 
 
-# TODO: the function that runs e.g. smallest. The inputs to this function are
-#  created by an iterator (input_iterate below). Make sure to create
-#  non-thread/multiprocess-safe objects (such as VQE-objects) here rather than
-#  in the iterator.
+# TODO: add your defined input functions to the dictionary below. The key is
+#  how many elements from the identifier the input function will be called
+#  with.
+input_functions = {0: input_0,
+                   3: input_3}
 
-# TODO: the arguments to simulate corresponds to:
-#  first_element_from_identifier_generator, second_element_from_id_gen, ...
-#  first_element_from_first_input_func, second_element_from_first_input_func, ..
-#  ...
-#  first_element_from_last_input_func, second_element_from_last_input_func
 
+# TODO: the function that runs e.g. smallest or vqe.run. Make sure to create
+#  non-multiprocess-safe objects (such as VQE-objects) here rather than
+#  in the input_n functions. Let id be an identifier; the arguments to this
+#  function will then be:
+#  id[0], id[1], ..., input_0()[0], input_0()[1], ..., input_1(id[0])[0],
+#  input_1(id[0])[1], ..., ..., input_N(id[0], id[1], ..., id[N-1])[0],
+#  input_N(id[0], id[1], ..., id[N-1])[1], ...
+#  (only including the defined input functions)
 def simulate(V, j, i, samples, dummy, h):
-    # TODO: create VQE-object here! (not multiprocess safe)
-    result = {'x': j+i+samples, 'fun': j*i*samples}
-    return result
+    # Use a broad try-except to don't crash if we don't have to
+    try:
+        # TODO: create VQE-object here! (not multiprocess safe)
+        # TODO: run e.g. smallest here and return result.
+        result = {'x': j+i+samples, 'fun': j*i*samples}
+        return result
+
+    except Exception as e:
+        # This will be saved in the metadata file so we can check success-rate.
+        return e
 
 
 class Bookkeeper:
     """
     Class for keeping track of what's been done and only assign new tasks.
     """
-    def __init__(self, iterator, book, callback=lambda: None,
-                 output_calc=None):
+    def __init__(self, iterator, book, output_calc=None):
         if output_calc is None:
             output_calc = {}
         self.iterator = iterator
         self.book = book
-        self.callback = callback
         self.output_calc = output_calc
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        x = self.iterator.__next__()
-        if x not in self.book or self.book[x] is not True:
-            self.book[x] = True
-            self.callback()
-            output = []
-            for i in range(len(x)+1):
-                if i in self.output_calc:
-                    output.extend(self.output_calc[i](
-                        *[y for j, y in enumerate(x) if j < i]))
-            yield x, output
+        while True:
+            x = self.iterator.__next__()
+            if x not in self.book or self.book[x] is not True:
+                self.book[x] = True
+                output = []
+                for i in range(len(x)+1):
+                    if i in self.output_calc:
+                        output.extend(self.output_calc[i](
+                            *[y for j, y in enumerate(x) if j < i]))
+                return x, output
 
 
 def wrap(x):
@@ -132,18 +158,26 @@ num_workers = os.cpu_count()
 max_task = 1
 chunksize = 1
 
-generator = Bookkeeper(identifier_generator, metadata['identifiers'],
-                       lambda: quick_save(path_metadata, metadata,
-                                          metametadata),
-                       {0: input_0,
-                        3: input_3})
+
+generator = Bookkeeper(identifier_generator(), metadata['ids'], input_functions)
 
 with Pool(num_workers, maxtasksperchild=max_task) as p:
     result_generator = p.imap_unordered(wrap, generator, chunksize=chunksize)
     for identifier, result in result_generator:
-        # Use identifier and result to determine file and append to that file
-        # using data.append
+        # Handle exceptions:
+        if isinstance(result, Exception):
+            # Save the error
+            data.append(path_metadata, [identifier, result])
+        else:
+            # Use identifier and result to determine file and append to that
+            # file using data.append
 
-        # Note that the results will be unordered so make sure to save enough
-        # info!
-        pass
+            # Note that the results will be unordered so make sure to save
+            # enough info!
+
+            # Mark the task as completed (last in the else, after saving result)
+            data.append(path_metadata, [identifier, True])
+
+
+# Post simulation.
+# TODO: Joel. Load metadata file. Go through and check success rate etc.
