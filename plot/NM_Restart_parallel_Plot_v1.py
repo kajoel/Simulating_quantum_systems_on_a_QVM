0890faa2b7d4a082ca2cc467e29f097f8ded47c3
Carl Eklind <carlekl@student.chalmers.se>
2019-04-26 14:35:29 +0200

    plot update

--- plot/NM_Restart_parallel_Plot_v1.py
+++ plot/NM_Restart_parallel_Plot_v1.py
@@ -10,18 +10,23 @@ import itertools
 from plot import tikzfigure
 
 
+nr_meas = 3000000
+procent = 1
+
 color_map = np.array(
     [(161, 218, 180), (65, 182, 196), (34, 94, 168), (37, 52, 148)]) / 255
 
 
 def tot_pts(meas_tol, fel, meas, fel_tol=1,):
-    nr_tot = [0 for i in range(9)]
+    nr_tot = [0 for i in range(len(list(max_para_range)))]
     for mat_idx in range(4):
-        for max_para in range(1, 10):
+        for max_para in max_para_range:
             for meas_, fel_ in zip(meas[mat_idx][max_para],fel[mat_idx][max_para]):
                 if meas_<meas_tol and fel_<fel_tol:
-                    nr_tot[max_para-1] +=1
+                    nr_tot[max_para-list(max_para_range)[0]] +=1
 
+    for i, max_para in enumerate(max_para_range):
+        nr_tot[i] /= sum(len(meas[mat_idx][max_para]) for mat_idx in range(4))
     return nr_tot
 
 
@@ -30,27 +35,44 @@ def plot_meas_fel(meas, fel, fig=1):
     for mat_idx in range(4):
         label = f'Matrix index: {mat_idx}'
 
-        for fig in range(1,10):
-            plt.subplot(3,3,fig)
-            plt.plot(meas[mat_idx][fig],fel[mat_idx][fig], 'o',markersize=2,
+        for max_para in max_para_range:
+            print(max_para)
+            fig = max_para - list(max_para_range)[0] +1
+            plt.subplot(3,1,fig)
+            plt.plot(meas[mat_idx][max_para],fel[mat_idx][max_para], 'o',markersize=2,
                      label=label, color=color_map[mat_idx])
-            plt.title(f'Maximum number of same parameters: {fig}')
+            plt.title(f'Maximum number of same parameters: {max_para}')
             #plt.xlabel('Antel mätningar')
             #plt.ylabel('Procentuellt fel')
             plt.legend()
-            plt.xlim(-100,3000000)
+            #plt.xlim(-100,3000000)
 
 
-size = 2
+size = 3
 version = 3
 
-base_dir = join(ROOT_DIR, f'data/NM_Restart_Parallel_v{version}')
+max_para_range = range(1)
+
+if version == 1:
+    max_para_range = range(3, 10)
+
+elif version == 2:
+    max_para_range = range(1, 10)
+
+elif version == 3 and size == 2:
+    max_para_range = range(1, 10)
+
+elif version == 3 and size == 3:
+    max_para_range = range(4, 7)
 
 
+base_dir = join(ROOT_DIR, f'data/NM_Restart_Parallel_v{version}/total')
+
+#len_H = [6, 8, 8, 6]
 
 nr_pts = {}
 
-fel = {mat_idx: {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: []}\
+fel = {mat_idx: {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: []} \
        for mat_idx in range(4)}
 
 meas = {mat_idx: {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: []}\
@@ -59,11 +81,12 @@ meas = {mat_idx: {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: []}
 for mat_idx in range(4):
     file = f'multi_particle_size={size}_matidx={mat_idx}.pkl'
     data_, metadata = data.load(file, base_dir)
+    print(len(data_))
 
     if version == 1:
         file2 = 'NM_Restart_Parallel_v1/fun_none/v1_multi_particle_size=2.pkl'
         eig, fun_none = data.load(file2)[0][0][mat_idx]
-        for max_para in range(3,10):
+        for max_para in max_para_range:
             for fun_none_ in fun_none[max_para]:
                 fel[max_para].append(np.abs((fun_none_-eig)/(-eig))*100)
 
@@ -71,21 +94,23 @@ for mat_idx in range(4):
     for i, tmp in enumerate(data_):
         identifier, result = tmp
         max_para = identifier[4]
-        meas[mat_idx][max_para].append(identifier[3] * result['fun_evals'])
-        fel[mat_idx][max_para].append(np.abs(result['fun_none']-result['correct'])*100)
+        #print(f"Eig: {result['correct']}, fun_none: {result['fun_none']}")
+        #print(result['status'])
+        meas[mat_idx][max_para].append(identifier[3]*result['fun_evals'])
+        fel[mat_idx][max_para].append(np.abs((result['fun_none']-result['correct'])/result['correct'])*100)
 
 
 plot_meas_fel(meas, fel)
 
 #tot_pts_tmp = np.asarray([])
 tot_pts_tmp = []
-meas_tol = np.arange(10000, 1000000, 10000)
+meas_tol = np.arange(1000, nr_meas, 1000)
 for meas_tol_ in meas_tol:
     #tot_pts_tmp = np.append(tot_pts_tmp, np.asarray(tot_pts(meas_tol_, fel, meas)))
-    tot_pts_tmp.append(tot_pts(meas_tol_, fel, meas, fel_tol=1))
+    tot_pts_tmp.append(tot_pts(meas_tol_, fel, meas, fel_tol=procent))
 
 plt.figure(2)
-legend = [f'{i}' for i in range(1,10)]
+legend = [f'{i}' for i in max_para_range]
 plt.plot(meas_tol, tot_pts_tmp)
 plt.legend(legend)
 plt.show()
