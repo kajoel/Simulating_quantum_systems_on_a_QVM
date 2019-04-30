782ecd146e1f733bfaa39f7d4972f2ba1281cc18
Carl Eklind <carlekl@student.chalmers.se>
2019-04-30 11:47:09 +0200

    UCC init_params

--- misc/UCC_zeros.py
+++ misc/UCC_zeros.py
@@ -3,19 +3,29 @@ from core import vqe_eig
 from core import init_params
 import numpy as np
 
-size = 3
+x = []
+norm = []
+matrix = 0
 ansatz_name = 'one_particle_ucc'
-h, eig = hamiltonians_of_size(size)
-initial_params = init_params.zeros(h[0].shape[0])
-initial_params = np.array([1,0])
-H, qc, ansatz_, _ = create_and_convert(ansatz_name,h[0])
-samples = 100000
-vqe = vqe_nelder_mead(samples=samples, H=H)
 
-print(eig[0])
+for size in range(2,7):
+    h, eig = hamiltonians_of_size(size)
+    initial_params = init_params.ucc(h[matrix].shape[0])
+    print(initial_params)
+    H, qc, ansatz_, _ = create_and_convert(ansatz_name,h[matrix])
+    #print(H)
+    samples = 10000
+    vqe = vqe_nelder_mead(fatol=1e-3)
 
-result = vqe_eig.smallest(H, qc,initial_params, vqe,ansatz_, samples = samples,
-                          return_all=True, disp=True, max_meas=samples*20)
+    #print(ansatz_(initial_params))
+    #print(eig[matrix])
 
-print(result)
+    result = vqe_eig.smallest(H, qc,initial_params, vqe,ansatz_, samples = samples,
+                              return_all=True, disp=True, max_meas=samples*20)
 
+    x.append(result['x'])
+    norm.append(np.linalg.norm(result['x']))
+
+for x_, norm_ in zip(x,norm):
+    print(x_)
+    print(norm_)
