3f659575c4edbb299d98cce108ca939471dfe6eb
Joel Karlsson <kajoel@student.chalmers.se>
2019-05-09 14:47:30 +0200

    Data fix

--- misc/split_data.py
+++ misc/split_data.py
@@ -8,11 +8,12 @@ def split(file=None):
     split_num = int(len(data)/2)
     save(file=name + '_part_1', data=data[:split_num], metadata=metadata,
          extract=True, base_dir='')
-    save(file=name + '_part_2', data=data[:split_num], metadata=metadata,
+    save(file=name + '_part_2', data=data[split_num:], metadata=metadata,
          extract=True, base_dir='')
 
 
 if __name__ == '__main__':
     split()
+
 
 
